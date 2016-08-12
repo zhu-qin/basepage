2211e2ca9121e09da43380d2c440a382c5b2758f
zhu-qin <qqinzhu@gmail.com>
2016-08-11 23:44:31 -0400

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-["Qin", "Larry", "Lord Commander"].each do |name|
+["Qin", "Larry", "Joe"].each do |name|
   User.create(username: name, password: "password", email: "fake@fake.com", main_project: 1)
 end
 
-Project.create(name: "Night's Watch", description: "Capstone Project", manager_id: 1)
-
-["Party!", "New Year!", "Christmas!"].each do |event|
-  Event.create(title: event, body:event, project_id: 1)
-end
-
-["Dog Pic"].each do |doc|
-  ProjectDocument.create(title: doc, project_id: 1)
-end
+Project.create(name: "Capstone Project", description: "Capstone Project", manager_id: 1)
