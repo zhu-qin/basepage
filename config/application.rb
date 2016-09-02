651294dd7524b434a52790df36d6f63e09843836
zhu-qin <qqinzhu@gmail.com>
2016-09-02 10:08:37 -0400

    fix calendar input

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module Basepage
   class Application < Rails::Application
 
-        config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
+      config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
